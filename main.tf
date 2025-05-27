6134dd28de95a33eccaa67e6694713dd4010c6dd
Akshay puri <52369586+Akshaypuri8380@users.noreply.github.com>
2025-05-27 22:04:38 +0530

    Add files via upload
    nginx-using-terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-resource "my_key_pair" "aishu_key"{
-  key_name = "aishu_key"
-  key_public = file("aishu_key.pub") 
+#ec2 instance with nginx and security group
+resource "aws_key_pair" "aishu_key" {
+  key_name   = "aishu_key"
+   public_key = file("C:/Users/ASUS/.ssh/id_rsa.pub")
+  tags = {
+    Name = "AishuKeyPair"
+  }
 }
+
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "DefaultVPC"
@@ -11,44 +16,42 @@ resource "aws_default_vpc" "default" {
 resource "aws_security_group" "aishu_sg" {
   name        = "aishu_sg"
   description = "Security group for Aishu instance"
-  vpc_id = aws_default_vpc.default.id
+  vpc_id      = aws_default_vpc.default.id
 
-  ingress = [
-    {
-      from_port   = 8000
-      to_port     = 8000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  ]
-  
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "AishuSecurityGroup"
@@ -57,13 +60,21 @@ resource "aws_security_group" "aishu_sg" {
 
 resource "aws_instance" "aishu_instance" {
   ami           = "ami-0e35ddab05955cf57"
-  key_name  = my_key_pair.aishu_key.key_name
-  security_groups = aws_security_group.aishu_sg.name
+  key_name      = aws_key_pair.aishu_key.key_name
+  security_groups = [aws_security_group.aishu_sg.name]
   instance_type = var.instance_type
   root_block_device {
     volume_size = 10
     volume_type = "gp3"
   }
+ user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install nginx -y
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
   tags = {
     Name = "Aishu_naginx_instance"
   }
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "instance_public_ip" {
-  value = aws_instance.aishu_instance.public_ip
+output "instance.ip" {
+    value = aws_instance.aishu_instance.public_ip
 }
